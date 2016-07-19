cd5efd4a8f89994c6be7b30053fc13ad70705512
Derek Chan <derekchan916@gmail.com>
2016-07-18 22:59:44 -0400

    Begin to add seed data for user and user images.

--- backend/WrestlerApp/db/seeds.rb
+++ backend/WrestlerApp/db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = [
+	{
+		"fname" => "Charmander",
+		"fb_id" => 10154237301715856,
+		"fb_access_token"  => "fake token",
+		"url" => "http://cdn.bulbagarden.net/upload/thumb/7/73/004Charmander.png/250px-004Charmander.png"
+	}
+]
+
+# Create users and their user images
+users.each do |user|
+	created_user = User.create!(
+		fname: user["fname"],
+		fb_id: user["fb_id"],
+		fb_access_token: user["fb_access_token"]
+	)
+
+	UserImage.create!(
+		user_id: created_user.id,
+		url: user["url"],
+		order: 1,
+		is_profile_image: true
+	)
+end
