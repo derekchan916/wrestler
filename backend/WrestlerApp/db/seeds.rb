a9ffbe9ceb72339864a3a3966b028695d8da6dfb
Derek Chan <derekchan916@gmail.com>
2016-07-19 22:34:13 -0400

    Add more seed data.

--- backend/WrestlerApp/db/seeds.rb
+++ backend/WrestlerApp/db/seeds.rb
@@ -6,13 +6,35 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# Flush database
+User.destroy_all
+UserImage.destroy_all
+
 users = [
 	{
 		"fname" => "Charmander",
-		"fb_id" => 10154237301715856,
-		"fb_access_token"  => "fake token",
+		"fb_id" => 1,
+		"fb_access_token"  => "fake token1",
 		"url" => "http://cdn.bulbagarden.net/upload/thumb/7/73/004Charmander.png/250px-004Charmander.png"
-	}
+	},
+	{
+		"fname" => "Squirtle",
+		"fb_id" => 2,
+		"fb_access_token"  => "fake token2",
+		"url" => "http://cdn.bulbagarden.net/upload/thumb/3/39/007Squirtle.png/250px-007Squirtle.png"
+	},
+	{
+		"fname" => "Bulbasaur",
+		"fb_id" => 3,
+		"fb_access_token"  => "fake token3",
+		"url" => "http://cdn.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/250px-001Bulbasaur.png"
+	},
+	{
+		"fname" => "Pikachu",
+		"fb_id" => 4,
+		"fb_access_token"  => "fake token4",
+		"url" => "http://cdn.bulbagarden.net/upload/thumb/0/0d/025Pikachu.png/250px-025Pikachu.png"
+	},
 ]
 
 # Create users and their user images
