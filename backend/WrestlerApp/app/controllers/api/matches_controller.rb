4067f75204f7f18536a1956e81b4971520e5aba0
Derek Chan <derekchan916@gmail.com>
2016-07-24 18:34:29 -0400

    Get the params working.

--- backend/WrestlerApp/app/controllers/api/matches_controller.rb
+++ backend/WrestlerApp/app/controllers/api/matches_controller.rb
@@ -3,17 +3,19 @@ require 'byebug'
 class Api::MatchesController < ApplicationController
 
 	def show
-		@users = User.find_by_sql(<<-SQL)
-			SELECT users_sec.*
+		@users = User.find_by_sql([
+			"SELECT users_sec.*
 			FROM users AS users_prime
 			JOIN users AS users_sec ON users_prime.id != users_sec.id
 			LEFT OUTER JOIN matches ON users_prime.id = matches.user_id
 			WHERE (matches.user_id IS NULL
 			OR users_sec.id != matches.match_id)
-			AND users_prime.id = 47
+			AND users_prime.id = ?
 			ORDER BY RANDOM()
-			LIMIT 5;
-		SQL
+			LIMIT ?;",
+			params[:id],
+			params[:count]
+		])
 
 		render "api/user/index"
 	end
