a66ffc84fa0cf926ee8bcd77b02e3b0441f8e109
Derek Chan <derekchan916@gmail.com>
2016-07-24 18:13:01 -0400

    Figure out how to add custom SQL query and next figure out how to fill in variables in the SQL.

--- backend/WrestlerApp/app/controllers/api/matches_controller.rb
+++ backend/WrestlerApp/app/controllers/api/matches_controller.rb
@@ -3,18 +3,18 @@ require 'byebug'
 class Api::MatchesController < ApplicationController
 
 	def show
-		query = "SELECT users_sec.*
+		@users = User.find_by_sql(<<-SQL)
+			SELECT users_sec.*
 			FROM users AS users_prime
 			JOIN users AS users_sec ON users_prime.id != users_sec.id
 			LEFT OUTER JOIN matches ON users_prime.id = matches.user_id
 			WHERE (matches.user_id IS NULL
 			OR users_sec.id != matches.match_id)
-			AND users_prime.id = 50
+			AND users_prime.id = 47
 			ORDER BY RANDOM()
-			LIMIT 5;"
+			LIMIT 5;
+		SQL
 
-		# @users = User.all
-		@users = ActiveRecord::Base.connection.execute(query)
 		render "api/user/index"
 	end
 
